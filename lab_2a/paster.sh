1b6f060f28e86b3d583b41e097976b356bbb1bbb
Rustam Yunusov <yrustt@yandex.ru>
2018-03-18 19:13:13 +0300

    Changed lab_2a

--- lab_2a/paster.sh
+++ lab_2a/paster.sh
@@ -4,9 +4,24 @@ GREEN_BACKGROUND="\033[42m"
 RED_BACKGROUND="\033[41m"
 YELLOW_BACKGROUND="\033[43m"
 WHITE_FONT_COLOR="\033[30m"
+
+GOOD_SETTINGS="${GREEN_BACKGROUND}${WHITE_FONT_COLOR}"
+WARNING_SETTINGS="${YELLOW_BACKGROUND}${WHITE_FONT_COLOR}"
+ERROR_SETTINGS="${RED_BACKGROUND}${WHITE_FONT_COLOR}"
 DEFAULT_SETTINGS="\033[0m"
 
+
 URL="http://paste.ee/api"
+
+
+function func_get_language() {
+    DATA="$1"
+    if [ -f "${DATA}" ]
+    then
+        local FILE_EXT=`echo "${DATA}" | awk -F. '{print $NF}'`
+        DATA=`cat ${DATA}`
+    fi
+}
 
 
 function func_post() {
@@ -16,24 +31,26 @@ function func_post() {
     local PASTE="$1"
     if [ -z "${PASTE}" ]
     then
-        local PASTE="import this"
-        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} Текст не задан. Значение по умолчанию: \"${PASTE}\". ${DEFAULT_SETTINGS}"
+        local PASTE="#!/usr/bin/awk -F"
+        echo -e "${WARNING_SETTINGS} Текст не задан. Значение по умолчанию: \"${PASTE}\". ${DEFAULT_SETTINGS}"
     fi
 
     local LANGUAGE="$2"
     if [ -z "${LANGUAGE}" ]
     then
         local LANGUAGE="python"
-        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} Язык не задан. Значение по умолчанию: \"${LANGUAGE}\". ${DEFAULT_SETTINGS}"
+        echo -e "${WARNING_SETTINGS} Язык не задан. Значение по умолчанию: \"${LANGUAGE}\". ${DEFAULT_SETTINGS}"
     fi
 
-    POST_RESULT=$(curl ${URL} -X "POST" --data "key=${KEY}&expire=${EXPIRE}&paste=${PASTE}&language=${LANGUAGE}" 2>/dev/null | \
+    POST_RESULT=$(curl ${URL} \
+                  -X "POST" \
+                  --data "key=${KEY}&expire=${EXPIRE}&paste=${PASTE}&language=${LANGUAGE}" 2>/dev/null | \
                   python3 -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['paste']['link']);" 2>/dev/null)
     if [ -n "${POST_RESULT}" ]
     then
-        echo -e "${GREEN_BACKGROUND}${WHITE_FONT_COLOR} Пост отправлен: \"${POST_RESULT}\". ${DEFAULT_SETTINGS}"
+        echo -e "${GOOD_SETTINGS} Пост отправлен: \"${POST_RESULT}\". ${DEFAULT_SETTINGS}"
     else
-        echo -e "${RED_BACKGROUND}${WHITE_FONT_COLOR} Ошибка при созданиии поста. ${DEFAULT_SETTINGS}"
+        echo -e "${ERROR_SETTINGS} Ошибка при созданиии поста. ${DEFAULT_SETTINGS}"
     fi
 }
 
@@ -43,7 +60,7 @@ function func_open() {
     if [ -z "${URL}" ]
     then
         local URL="http://simple-fauna.ru/wp-content/uploads/2017/03/xarakter-velsh-korgi-pembrok.jpg"
-        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} URL не передан. ${DEFAULT_SETTINGS}"
+        echo -e "${WARNING_SETTINGS} URL не передан. ${DEFAULT_SETTINGS}"
     fi
 
     xdg-open "${URL}"
@@ -51,8 +68,18 @@ function func_open() {
 
 
 function func_pipeline() {
-    func_post
+    func_get_language "$1"
+    func_post "${DATA}"
     func_open ${POST_RESULT}
 }
 
-func_pipeline
+
+if [ $# -eq 0 ]
+then
+    func_pipeline
+elif [ $# -eq 1 ]
+then
+    func_pipeline $1
+else
+    echo "1"
+fi
