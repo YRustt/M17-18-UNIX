3a2f5e126872c72bc224e10d7139a25ed07f9294
Rustam Yunusov <yrustt@yandex.ru>
2018-03-18 17:57:06 +0300

    Added labs 1 and 2a

---
+++ lab_2a/paster.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+GREEN_BACKGROUND="\033[42m"
+RED_BACKGROUND="\033[41m"
+YELLOW_BACKGROUND="\033[43m"
+WHITE_FONT_COLOR="\033[30m"
+DEFAULT_SETTINGS="\033[0m"
+
+URL="http://paste.ee/api"
+
+function func_post() {
+    local KEY="public"
+    local EXPIRE="228"
+    local PASTE="${1:-paste}"
+    local LANGUAGE="${2:-python}"
+
+    POST_RESULT=$(curl ${URL} -X "POST" --data "key=${KEY}&expire=${EXPIRE}&paste=${PASTE}&language=${LANGUAGE}" 2>/dev/null | \
+                   python3 -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['paste']['asd']);" 2>/dev/null)
+    if [ -n "${POST_RESULT}" ]
+    then
+        echo -e "${GREEN_BACKGROUND}${WHITE_FONT_COLOR} Пост отправлен: ${POST_RESULT} ${DEFAULT_SETTINGS}"
+    else
+        echo -e "${RED_BACKGROUND}${WHITE_FONT_COLOR} Ошибка при созданиии поста ${DEFAULT_SETTINGS}"
+    fi
+}
+
+function func_open() {
+    local URL="$1"
+    if [ -z "${URL}" ]
+    then
+        local URL="http://simple-fauna.ru/wp-content/uploads/2017/03/xarakter-velsh-korgi-pembrok.jpg"
+        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} URL не передан ${DEFAULT_SETTINGS}"
+    fi
+
+    xdg-open "${URL}"
+}
+
+function func_pipeline() {
+    func_post
+    func_open ${POST_RESULT}
+}
+
+func_pipeline
