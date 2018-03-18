2ef8d8d20f1de8f86a89471e8a2c98afbd1e04c0
Rustam Yunusov <yrustt@yandex.ru>
2018-03-18 18:03:54 +0300

    Changed lab_2a

--- lab_2a/paster.sh
+++ lab_2a/paster.sh
@@ -8,32 +8,47 @@ DEFAULT_SETTINGS="\033[0m"
 
 URL="http://paste.ee/api"
 
+
 function func_post() {
     local KEY="public"
     local EXPIRE="228"
-    local PASTE="${1:-paste}"
-    local LANGUAGE="${2:-python}"
+
+    local PASTE="$1"
+    if [ -z "${PASTE}" ]
+    then
+        local PASTE="import this"
+        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} Текст не задан. Значение по умолчанию: \"${PASTE}\". ${DEFAULT_SETTINGS}"
+    fi
+
+    local LANGUAGE="$2"
+    if [ -z "${LANGUAGE}" ]
+    then
+        local LANGUAGE="python"
+        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} Язык не задан. Значение по умолчанию: \"${LANGUAGE}\". ${DEFAULT_SETTINGS}"
+    fi
 
     POST_RESULT=$(curl ${URL} -X "POST" --data "key=${KEY}&expire=${EXPIRE}&paste=${PASTE}&language=${LANGUAGE}" 2>/dev/null | \
-                   python3 -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['paste']['asd']);" 2>/dev/null)
+                  python3 -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['paste']['link']);" 2>/dev/null)
     if [ -n "${POST_RESULT}" ]
     then
-        echo -e "${GREEN_BACKGROUND}${WHITE_FONT_COLOR} Пост отправлен: ${POST_RESULT} ${DEFAULT_SETTINGS}"
+        echo -e "${GREEN_BACKGROUND}${WHITE_FONT_COLOR} Пост отправлен: \"${POST_RESULT}\". ${DEFAULT_SETTINGS}"
     else
-        echo -e "${RED_BACKGROUND}${WHITE_FONT_COLOR} Ошибка при созданиии поста ${DEFAULT_SETTINGS}"
+        echo -e "${RED_BACKGROUND}${WHITE_FONT_COLOR} Ошибка при созданиии поста. ${DEFAULT_SETTINGS}"
     fi
 }
+
 
 function func_open() {
     local URL="$1"
     if [ -z "${URL}" ]
     then
         local URL="http://simple-fauna.ru/wp-content/uploads/2017/03/xarakter-velsh-korgi-pembrok.jpg"
-        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} URL не передан ${DEFAULT_SETTINGS}"
+        echo -e "${YELLOW_BACKGROUND}${WHITE_FONT_COLOR} URL не передан. ${DEFAULT_SETTINGS}"
     fi
 
     xdg-open "${URL}"
 }
+
 
 function func_pipeline() {
     func_post
